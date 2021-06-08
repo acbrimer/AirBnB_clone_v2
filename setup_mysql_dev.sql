0187b15888ae77e0c59c0f9c3574c9d8758fcf5c
Adam Brimer <acbrimer@github.com>
2021-06-08 22:27:21 +0000

    Fix for task 3-- fixed grant all for user'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 ALTER USER 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbtn_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbtn_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
+FLUSH PRIVILEGES;

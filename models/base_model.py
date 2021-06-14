c3ad587c5288296a31a6264f8e3a0e044ceea555
Adam Brimer <acbrimer@github.com>
2021-06-14 03:49:39 +0000

    Fixes for task 10

--- models/base_model.py
+++ models/base_model.py
@@ -16,17 +16,14 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+        kwargs['id'] = str(uuid.uuid4())
+        kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            '%Y-%m-%dT%H:%M:%S.%f') if 'created_at' in kwargs else datetime.now()
+        kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            '%Y-%m-%dT%H:%M:%S.%f') if 'updated_at' in kwargs else datetime.now()
+        if '__class__' in kwargs:
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/place.py
+++ models/place.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.review import Review
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+import os
 
 place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id')),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'))
+    Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
 )
 
 class Place(BaseModel, Base):
@@ -25,23 +25,15 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable = False, default=0)
     latitude = Column(Float, nullable = True)
     longitude = Column(Float, nullable = True)
-    reviews = relationship('Review', back_populates='reviews')
+    reviews = relationship('Review', backref='place')
     amenities = relationship('Amenity',
                     viewonly=False,
                     secondary=place_amenity,
-                    backref='amenities')
+                    backref='amenity'
+                    )
     amentity_ids = []
 
-    @property
-    def reviews(self):
-        from models import storage
-        return { key: val for key, val in storage.all(Review).items() if val['user_id'] == self.id }
-
-    @property
-    def amenities(self):
-        from models import storage
-        return { key: val for key, val in storage.all(Amenity).items() if val['id'] in self.amentity_ids }
     
-    @amenities.setter
-    def amenities(self, val):
-        self.amentity_ids.append(val)
+
+
+   

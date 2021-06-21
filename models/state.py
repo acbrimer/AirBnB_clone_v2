8b8fb9fdc503fdc558ceb8af0fa003a930094f3b
Adam <arbimer@mac.com>
2021-06-21 15:18:43 -0500

    Fix for state

--- models/state.py
+++ models/state.py
@@ -5,18 +5,19 @@ from models.base_model import Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import os
 
 class State(BaseModel, Base):
 
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
-
-    @property
-    def cities(self):
-        from models import storage
-        c = storage.all(City).items()
-        return {key: val for key, val in c if val['state_id'] == self.id}
+    if os.getenv('HBNB_TYPE_STORAGE', '') == 'db':
+        cities = relationship('City', backref='state',
+                            cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            c = storage.all(City).items()
+            return {key: val for key, val in c if val['state_id'] == self.id}

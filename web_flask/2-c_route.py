1f0560d3b3bbf651b013b62232c602510bb6fa1f
Adam <arbimer@mac.com>
2021-06-21 10:57:26 -0500

    Fix for task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,13 +11,16 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def c(text):
-    return "C ".format(text.replace('_', ' '))
+    return "C {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.url_map.strict_slashes = False

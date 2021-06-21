17603c33bf8aeaed87148f0c6787dbd883d7a737
Adam <arbimer@mac.com>
2021-06-21 10:52:18 -0500

    Added web_flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for a simple flask app with single route """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module for a simple flask app with single route """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    return "C ".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port="5000")

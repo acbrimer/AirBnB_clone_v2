a034162508dbf5af6e9d11de8e298847bb3bc12c
Adam <arbimer@mac.com>
2021-06-21 14:29:55 -0500

    Added solution for 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for a simple flask app with single route """
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states')
+def states_list():
+    states = list(storage.all(State).values())
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port="5000")

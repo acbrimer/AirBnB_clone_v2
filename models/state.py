8353b677206f88ef8f95185ef0a32625b009b694
Adam <arbimer@mac.com>
2021-06-21 15:00:01 -0500

    Fix for state.cities

--- models/state.py
+++ models/state.py
@@ -12,7 +12,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='cities')
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
 
     @property
     def cities(self):

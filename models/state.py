0e426d90af94fc3045664bdf1aad5ad3c8c63b51
Adam Brimer <acbrimer@github.com>
2021-06-21 18:21:54 +0000

    Fix for State model-- removed unique constraint

--- models/state.py
+++ models/state.py
@@ -11,7 +11,7 @@ class State(BaseModel, Base):
 
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
     cities = relationship('City', back_populates='cities')
 
     @property
